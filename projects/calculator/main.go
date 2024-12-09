03e983dc95d26eb4cf45bb517534f528960cc474
Jabrail <dzabrail.aidiev@mail.ru>
2024-12-10 01:50:18 +0300

    Add projects

--- projects/calculator/main.go
+++ projects/calculator/main.go
@@ -1,7 +1,37 @@
 package main
 
-// реализовать calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+import "fmt"
+
+func calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int {
+	out := make(chan int)
+	var tmp int
+	go func() {
+		defer close(out)
+		select {
+		case tmp = <-firstChan:
+			out <- tmp * tmp
+			return
+		case tmp = <-secondChan:
+			out <- tmp * 3
+			return
+
+		case <-stopChan:
+			return
+		}
+		
+	}()
+	
+	return out
+
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+	firstChan := make(chan int)
+	secondChan := make(chan int)
+	stopChan := make(chan struct{})
+	go func(){
+		secondChan <- 8
+	}()
+	fmt.Println(<-calculator(firstChan, secondChan, stopChan))
+
 }
--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,37 @@
 package main
 
-// реализовать removeDuplicates(in, out chan string)
+import "fmt"
+
+func removeDuplicates(inputStream, outputStream chan string) {
+	prev := <-inputStream
+	outputStream <- prev
+	for i := range inputStream {
+		if prev != i {
+			outputStream <- i
+		}
+		prev = i
+	}
+	close(outputStream)
+
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+
+	input := make(chan string)
+	output := make(chan string)
+	go removeDuplicates(input, output)
+	go func() {
+		var a, b, c, d, e string
+		fmt.Scan(&a, &b, &c, &d, &e)
+		input <- a
+		input <- b
+		input <- c
+		input <- d
+		input <- e
+		close(input)
+	}()
+	for i := range output {
+		fmt.Println(i)
+	}
+
 }
